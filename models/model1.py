2e3991e639f4d35cfa6932a3ec8fd1193077387c
DanielJunCho <djcho@hamilton.edu>
2024-07-22 02:40:07 +0000

    Architecture changes and validation data

--- models/model1.py
+++ models/model1.py
@@ -8,27 +8,60 @@ import numpy as np
 
 
 class CNN(nn.Module):
+    # def __init__(self):
+    #     super(CNN, self).__init__()
+    #     self.CNN1 = nn.Conv1d(1, 64, kernel_size=4, stride=2)
+    #     self.Dropout1 = nn.Dropout(0.2)
+    #     self.CNN2 = nn.Conv1d(64, 32, kernel_size=4, stride=2)
+    #     self.Dropout2 = nn.Dropout(0.2)
+    #     self.AdaptivePool = nn.AdaptiveAvgPool1d(1)
+    #     self.FC = nn.Linear(32, 1)
+    #     self.sigmoid = nn.Sigmoid()
+        
+    # def forward(self, x):
+    #     x = x.unsqueeze(1)
+    #     x = self.CNN1(x)
+    #     x = self.Dropout1(x)
+    #     x = self.CNN2(x)
+    #     x = self.Dropout2(x)
+    #     x = self.AdaptivePool(x)
+    #     x = x.squeeze()
+    #     x = self.FC(x)
+    #     x = self.sigmoid(x)
+    #     return x
+
     def __init__(self):
         super(CNN, self).__init__()
-        self.CNN1 = nn.Conv1d(1, 64, kernel_size=4, stride=2)
-        self.Dropout1 = nn.Dropout(0.2)
-        self.CNN2 = nn.Conv1d(64, 32, kernel_size=4, stride=2)
-        self.Dropout2 = nn.Dropout(0.2)
-        self.AdaptivePool = nn.AdaptiveAvgPool1d(1)
-        self.FC = nn.Linear(32, 1)
-        self.sigmoid = nn.Sigmoid()
+        
+        self.features = nn.Sequential(
+            nn.Conv1d(1, 64, kernel_size=4, stride=2),
+            nn.BatchNorm1d(64),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.2),
+            
+            nn.Conv1d(64, 128, kernel_size=4, stride=2),
+            nn.BatchNorm1d(128),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.2),
+            
+            nn.Conv1d(128, 256, kernel_size=4, stride=2),
+            nn.BatchNorm1d(256),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.2),
+        )
+
+        self.lstm = nn.LSTM(input_size=256, hidden_size=128, num_layers=1, batch_first=True)
+        self.classifier = nn.Linear(128, 1)
         
     def forward(self, x):
-        x = x.unsqueeze(1)
-        x = self.CNN1(x)
-        x = self.Dropout1(x)
-        x = self.CNN2(x)
-        x = self.Dropout2(x)
-        x = self.AdaptivePool(x)
-        x = x.squeeze()
-        x = self.FC(x)
-        x = self.sigmoid(x)
-        return x
+        x = x.unsqueeze(1) if x.dim() == 2 else x  # handle 2D input
+        x = self.features(x)
+        x = x.permute(0, 2, 1)
+        _, (h_n, _) = self.lstm(x)
+        x = h_n[-1]
+        x = self.classifier(x)
+        return torch.sigmoid(x)
+
 
 
 class trainCNN:
@@ -41,19 +74,24 @@ class trainCNN:
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=learning_rate)
         self.loss = nn.BCELoss()
 
-    def fit(self, X_train, y_train, epochs=50, batch_size=16, validation_data=None):
+    def fit(self, X_train, y_train, epochs=50, batch_size=64, val_threshold=5, validation_data=None):
         train_dataset = TensorDataset(torch.FloatTensor(X_train), torch.FloatTensor(y_train))
         train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
+
         if validation_data:
-            print("validation_data")
-            # gotta make a new dataset for validation data as well for evaluation time
+            X_val, y_val = validation_data
+            val_dataset = TensorDataset(torch.FloatTensor(X_val), torch.FloatTensor(y_val))
+            val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+
+        best_val_loss = float('inf')
+        epochs_no_improvement = 0
+        best_model = None
 
         for epoch in range(epochs):
             running_loss = 0.0
             for i, (inputs, labels) in enumerate(train_loader, 0):
                 inputs, labels = inputs.to(self.device), labels.to(self.device)
-                
 
                 self.optimizer.zero_grad()
 
@@ -65,9 +103,37 @@ class trainCNN:
                 self.optimizer.step()
 
                 running_loss = loss.item()
-                print(f'[epoch: {epoch + 1}, iteration: {i + 1:5d}] loss: {running_loss}')
-                running_loss = 0.0
+                if i % 10 == 0:  # Print every 10 iterations
+                    print(f'[epoch: {epoch + 1}, iteration: {i}] loss: {running_loss / 10:.4f}')
+                    running_loss = 0.0
 
+            if validation_data:
+                self.model.eval()
+                val_loss = 0.0
+                with torch.no_grad():
+                    for inputs, labels in val_loader:
+                        inputs, labels = inputs.to(self.device), labels.to(self.device)
+                        outputs = self.model(inputs)
+                        outputs = outputs.squeeze()
+                        loss = self.loss(outputs, labels)
+                        val_loss += loss.item()
+
+
+                val_loss /= len(val_loader)
+                print(f'[epoch: {epoch + 1}] validation loss: {val_loss:.4f}')
+
+                if val_loss < best_val_loss:
+                    best_val_loss = val_loss
+                    epochs_no_improvement = 0
+                    best_model = self.model.state_dict()
+                else:
+                    epochs_no_improvement += 1
+                
+                if epochs_no_improvement == val_threshold:
+                    print(f"Early stopping triggered after {epoch + 1} epochs")
+                    break
+
+        self.model.load_state_dict(best_model)
         print("Finished training")
 
     def eval(self, X_test, y_test):
@@ -78,7 +144,6 @@ class trainCNN:
             outputs = self.model(X_test)
             outputs = outputs.squeeze()
 
-        print(outputs, y_test)
         predictions = (outputs > 0.5).float()
         y_test = y_test.long()
         
@@ -90,10 +155,13 @@ class trainCNN:
         return accuracy.item(), precision.item(), recall.item()
 
 def train_model(features, labels):
-    X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
+    scaler = StandardScaler()
+    features_scaled = scaler.fit_transform(features)
 
+    X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
 
     model = trainCNN()
     model.compile()
-    model.fit(X_train, y_train)
+    model.fit(X_train, y_train, validation_data=(X_val, y_val))
     accuracy, precision, recall = model.eval(X_test, y_test)
