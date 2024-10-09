9971d8745435f41ca182512536dc1ba626efab93
Daniel Cho <116980842+DanielJunsangCho@users.noreply.github.com>
2024-10-09 14:06:32 -0400

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import preprocessing.audio as Audio
-import os
-
-for file in os.listdir("samples/train"):
-    file = os.path.join("samples/train", file)
-    Audio.mp3_to_wav(file)
-
-
-print("yay")
